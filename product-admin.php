12f8ec6c9c333149257227516543a5632184e862
jeay <wrjie@msn.cn>
2017-11-14 17:14:06 +0800

    登录验证

--- product-admin.php
+++ product-admin.php
@@ -6,6 +6,7 @@
  * @Last Modified time: 2017-11-11 15:27:19
  */
 session_start();
+$psw = '12';
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -15,7 +16,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap -->
-    <link href="https://cdn.bootcss.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -24,23 +25,27 @@ session_start();
       <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
     <?php/*  if ( $_SERVER['SERVER_ADDR'] !="127.0.0.1" && !isset($_SESSION['psw'])) { */?>
-    <?php if ( !isset($_SESSION['psw'])) {?>
+    <?php if ( empty($_SESSION['psw']) && empty($_POST['psw']) )  {?>
     <title>请验证身份</title>
+    <?php } else {?>
+    <title>产品管理</title>
+    <?php }?>
 </head>
 <body>
+    <?php if ( !isset($_SESSION['psw']) )  {?>
+        <?php if ( empty($_POST['psw']) || $_POST['psw']!=$psw ) { ?>
     <div class="container">
         <form method="post">
-            <div class="form-group">
-                <label for="Password">请验证身份</label>
-                <input type="password" class="form-control" id="Password" placeholder="Password">
+            <div class="form-group <?php if ( !empty($_POST['psw']) && $_POST['psw']!=$psw) { echo 'has-error'; } ?>">
+                <label class="control-label" for="Password"><?php if ( !empty($_POST['psw']) && $_POST['psw']!=$psw) { echo '密码错误'; } ?></label>
+                <input type="password" name='psw' class="form-control " id="Password" placeholder="Password">
             </div>
             <button type="submit" class="btn btn-default">提交</button>
         </form>
     </div>
+        <?php } ?>
     <?php } else {?>
-    <title>产品管理</title>
-</head>
-<body>
+
 
 
     <?php }?>
@@ -48,6 +53,6 @@ session_start();
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://cdn.bootcss.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
 </html>
